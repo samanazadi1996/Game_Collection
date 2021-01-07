927df0d8d9ba45b0cff2b6d346071f8821e51a61
saman azadi <samanazadi1996@gmail.com>
2021-01-07 22:12:41 +0330

    Add Home Page and inset icons

--- AngularProject/src/app/home/home.component.ts
+++ AngularProject/src/app/home/home.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    setInterval(() => {
+      if (this.timerEnable) {
+
+        this.slideIndex += this.ind;
+        var x = document.getElementsByClassName("mySlides");
+        if (this.slideIndex > x.length) { this.slideIndex = 1 }
+        if (this.slideIndex < 1) { this.slideIndex = x.length }
+      }
+    }, 3000);
+   }
 
   ngOnInit(): void {
+  }
+  
+  slideIndex = 1;
+  ind = 1
+  timerEnable = true
+  slider(n) {
+    this.timerEnable = false
+    this.ind = n;
+    this.slideIndex += n;
+    var x = document.getElementsByClassName("mySlides");
+    if (this.slideIndex > x.length) { this.slideIndex = 1 }
+    if (this.slideIndex < 1) { this.slideIndex = x.length }
+    setTimeout(() => {
+      this.timerEnable = true;
+    }, 3000);
   }
 
 }
