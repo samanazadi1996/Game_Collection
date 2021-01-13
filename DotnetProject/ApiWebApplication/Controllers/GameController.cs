498e9715e3e7164ec51a4549a48902e98a0697e1
saman azadi <samanazadi1996@gmail.com>
2021-01-13 20:31:47 +0330

    add ApiWebApplication project

---
+++ DotnetProject/ApiWebApplication/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiWebApplication.Controllers
+{
+    [ApiController]  
+    [Route("api/[controller]/[action]")]
+    public class GameController : ControllerBase
+    {
+        public string Get()
+        {
+            return "/Game1/Index.html";
+        }
+
+    }
+}
